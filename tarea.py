013d02175e5e3f320b8fef156b3a99212f1c10f4
of-curso-python <of.curso.python@gmail.com>
2017-06-15 19:30:04 -0600

    I-1

--- tarea.py
+++ tarea.py
@@ -1,6 +1,11 @@
-nombre = input("digita tu name")
-apellido = input("digita tu apellido")
-edad = int(input("deigita tu edad"))
-cumple = 100 - edad 
-cumplira = 2017 + cumple
-print ("<<", nombre,">>, cumplira 100 anos en el :",str(cumplira))
+
+
+if __name__ == '__main__':
+    nombre = input("digita tu name")
+    apellido = input("digita tu apellido")
+    edad = int(input("deigita tu edad"))
+    cumple = 100 - edad
+    cumplira = 2017 + cumple
+    mensaje = "<< {nombre} >>, cumplira 100 en el: {anio}"
+
+    print(mensaje.format(nombre=nombre, anio=cumplira))
